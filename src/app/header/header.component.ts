039cd03fc682567101e743821f7e911293eeb545
nickariley <nickariley@gmail.com>
2020-08-18 15:25:31 -0500

    added shopping cart property of iproduct array, injected productService, onInit getShoppingCart method invoked

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IProduct } from '../model/iproduct';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  shoppingCart: IProduct[] = [];
+
+  constructor(private productService: ProductService) { }
 
   ngOnInit() {
+    this.shoppingCart = this.productService.getShoppingCart();
   }
 
 }
