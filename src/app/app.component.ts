99065537ed705b111d4e1e5fb34985d5aedc6710
nickariley <nickariley@gmail.com>
2020-08-18 15:14:07 -0500

    App.Component imported IProduct and ProductService, Implements OnInit, Injected ProductService in constructor, onInit GetProducts method invoked
    ProductDetail removed hard coded data passed in Product properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './model/iproduct';
+import { ProductService } from './services/product.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  product: IProduct[] = [];
+
+  constructor(private productService: ProductService) {
+
+  }
+
+  ngOnInit(): void {
+    this.product = this.productService.getProducts();
+  }
 }
--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { IProduct } from '../model/iproduct';
 
 @Component({
   selector: 'app-product-detail',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailComponent implements OnInit {
 
+  @Input() product: IProduct;
   constructor() { }
 
   ngOnInit() {
